4082c130bc9263f3c0288c9ee9a87ca48ee0382e
stassev <stassev@runningonphysics.org>
2023-03-14 09:37:24 -0400

    Update simplex.py

--- N-dim/simplex.py
+++ N-dim/simplex.py
@@ -1,3 +1,8 @@
+#**Copyright:** Svetlin Tassev (2022-2023)
+#**License:** GNU General Public License v3.0
+#This file is part of Mechanical-Linkage-Neural-Network (https://github.com/stassev/Mechanical-Linkage-Neural-Network).
+
+
 from numpy.linalg import det
 import numpy as np
 from math import factorial 
